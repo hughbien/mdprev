48dbfb07da93c872d58c6cee52ed41b48bac461e
Hugh Bien <hugh@hughbien.com>
2011-06-22 21:18:43 -0700

    Initial markdown preview commit.

---
+++ mkdprev_spec.rb
@@ -0,0 +1,54 @@
+require 'mkdprev'
+
+describe MarkdownPreview do
+  it 'should build anchors from markdown' do
+    body, anchors = MarkdownPreview.build_body_and_anchors("
+      # Header !@%^_ 1
+      Paragraph
+      # Header 2
+      Paragraph 2
+    ".gsub(/^\s+/, ''))
+    anchors.should == [
+      ['header-1', 'Header !@%^_ 1'],
+      ['header-2', 'Header 2']
+    ]
+  end
+
+  it 'should build body from markdown' do
+    body, anchors = MarkdownPreview.build_body_and_anchors("
+      # Header
+      Sample Text
+    ".gsub(/^\s+/, ''))
+    body.should =~ /<h1>Header<\/h1>/
+    body.should =~ /<p>Sample Text<\/p>/
+  end
+
+  it 'should generate title from filenames' do
+    MarkdownPreview.title_from_files(['sample.mkd']).should == 'Sample'
+    MarkdownPreview.title_from_files([
+      'league-of-extraordinary-_^Gentlemen',
+      'ignoreothernames'
+    ]).should == 'League Of Extraordinary Gentlemen'
+  end
+
+  it 'should build nav from anchors' do
+    nav = MarkdownPreview.build_nav([
+      ['first-id', 'First ID'],
+      ['second-item', 'Second Item'],
+      ['third', 'Third'],
+      ['fourth', 'Fourth'],
+      ['fifth', 'Fifth']
+    ])
+    nav.should =~ /^<ul id="main-nav">/
+    nav.should =~ /<li><a href="#first-id">First ID<\/a><\/li>/
+    nav.should =~ /<li><a href="#second-item">Second Item<\/a><\/li>/
+    nav.should =~ /<li><a href="#third">Third<\/a><\/li>/
+    nav.should =~ /<li><a href="#fourth">Fourth<\/a><\/li>/
+    nav.should =~ /<\/ul>$/
+  end
+
+  it 'should determine nav class from amount of anchors' do
+    MarkdownPreview.nav_class([]).should == 'no-nav'
+    MarkdownPreview.nav_class(Array.new(10)).should == ''
+  end
+end
